85685c368e5e358b85db3911100f811c6d506d07
William Mukesh <william.mukesh@ithands.biz>
2017-06-15 18:57:50 +0530

    Create migration

---
+++ database/migrations/2017_06_15_131730_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('Ttitle');
+            $table->string('Tbody');
+            $table->DateTime('Tstart');
+            $table->DateTime('Tend');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
