699a849cffb1cd6392438cf418cb575c4916e0e8
William Mukesh <william.mukesh@ithands.biz>
2017-06-16 11:06:47 +0530

    add user seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if (config('app.env') !== 'production') {
+            $this->call(UsersTableSeeder::class);
+        }
     }
 }
